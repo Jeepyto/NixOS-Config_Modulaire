f0a4ce3437a4320e7063d9a8b4d978615732e9c5
Jeepyto <charrierjp85@gmail.com>
2026-07-05 04:46:24 +0200

    feat: Mettre à jour les paramètres de contrôle du filtre de réduction de bruit RNNoise

--- modules/system/micro-filter.nix
+++ modules/system/micro-filter.nix
@@ -32,8 +32,8 @@ in
                     plugin = librnnoise_ladspa
                     label = noise_suppressor_mono
                     control = {
-                      "VAD Threshold (%)" = 80.0
-                      "VAD Grace Period (ms)" = 350
+                      "VAD Threshold (%)" = 50.0
+                      "VAD Grace Period (ms)" = 200
                       "Retroactive VAD Grace (ms)" = 0
                     }
                   }
